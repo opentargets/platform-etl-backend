6e6bb97baf2d2a1e5d42b91fc6a116d6d89519b8
Cinzia Malangone <cinzia@ebi.ac.uk>
2019-11-12 14:14:44 +0000

    Diseases ES Loader

--- elasticsearch/load_drugs.sh
+++ elasticsearch/load_drugs.sh
@@ -1,1 +1,1 @@
-cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index drugs --type drug json --json-lines -
+cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index drugs --type drug --id-field id json --json-lines -
