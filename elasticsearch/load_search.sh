e87cb0ad589a1333439f1e640090228d2887b94b
Cinzia Malangone <cinzia@ebi.ac.uk>
2019-12-03 10:14:15 +0000

    Fixing typo

--- elasticsearch/load_search.sh
+++ elasticsearch/load_search.sh
@@ -1,1 +1,1 @@
-cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings_search.json" --with-retry K--bulk-size 5000 --index searches --type search json --json-lines -
+cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings_search.json" --with-retry --bulk-size 5000 --index searches --type search --id-field id json --json-lines -
