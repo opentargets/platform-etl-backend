90675bc932d4e89a7e8b8d4f79c647bfabd13a27
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-02-24 13:53:53 +0000

    New files to load the indices

--- elasticsearch/load_cancerbiomarker.sh
+++ elasticsearch/load_cancerbiomarker.sh
@@ -1,1 +1,10 @@
-cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index cancerbiomarker --type cancerbiomarker json --json-lines -
+#!/bin/bash
+#cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index cancerbiomarker --type cancerbiomarker json --json-lines -
+
+export INDEX_SETTINGS="index_settings.json"
+export INDEX_NAME="20.02_cancerbiomarker"
+export TYPE_FIELD="cancerbiomarker"
+export INPUT="../out/cancerBiomarkers"
+export ES="http://localhost:9200"
+
+./load_jsons.sh
