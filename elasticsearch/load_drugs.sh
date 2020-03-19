4e52de835e0984717fb3c47df520fdd897cf23c2
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-03-19 11:54:46 +0000

    Loading scripts

--- elasticsearch/load_drugs.sh
+++ elasticsearch/load_drugs.sh
@@ -1,1 +1,10 @@
-cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index drugs --type drug --id-field id json --json-lines -
+#cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index drugs --type drug --id-field id json --json-lines -
+
+export INDEX_SETTINGS="index_settings.json"
+export INDEX_NAME="20.02_drug"
+export TYPE_FIELD="drug"
+export INPUT="../out/drugs"
+export ES="http://localhost:9200"
+export ID="id"
+
+./load_jsons.sh
