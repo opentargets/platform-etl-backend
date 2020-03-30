8c8ee0af431881e00b85967e0785d274f8495e80
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-03-30 11:20:44 +0100

    load es

--- elasticsearch/load_evidence_direct.sh
+++ elasticsearch/load_evidence_direct.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export INDEX_SETTINGS="index_settings.json"
-export INDEX_NAME="20.02_direct_evidence"
+export INDEX_NAME="20.02_evidence_direct"
 export TYPE_FIELD="ev_direct"
 export INPUT="../out/evidenceDrugDirect"
 export ES="http://localhost:9200"
