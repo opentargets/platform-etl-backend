76b74689a611978e5f759e3b457ded261ee960e8
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-04-15 12:48:48 +0100

    Release prefix

--- elasticsearch/load_cancerbiomarker.sh
+++ elasticsearch/load_cancerbiomarker.sh
@@ -2,7 +2,8 @@
 #cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index cancerbiomarker --type cancerbiomarker json --json-lines -
 
 export INDEX_SETTINGS="index_settings.json"
-export INDEX_NAME="20.02_cancerbiomarker"
+export RELEASE='20.04_'
+export INDEX_NAME="cancerbiomarker"
 export TYPE_FIELD="cancerbiomarker"
 export INPUT="../out/cancerBiomarkers"
 export ES="http://localhost:9200"
