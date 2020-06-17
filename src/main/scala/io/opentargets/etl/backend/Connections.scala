c807eb86df0ef4805960f6cb4631464b05aaad84
Miguel C <mkarmona@gmail.com>
2020-06-17 15:44:59 +0100

    wip: include edges for diseases

--- src/main/scala/io/opentargets/etl/backend/Connections.scala
+++ src/main/scala/io/opentargets/etl/backend/Connections.scala
@@ -9,28 +9,36 @@ import org.apache.spark.sql.functions._
 object Connections extends LazyLogging {
   def diseaseEdges(df: DataFrame): DataFrame = {
     df.selectExpr(
-      "id",
-      "parents"
-    ).withColumn("type", lit("relation"))
+        "id",
+        "parents"
+      )
+      .withColumn("parent", explode(col("parents")))
+      .where(col("parent").isNotNull)
+      .withColumn("type", lit("relation"))
       .withColumn("cat", lit("parent_of"))
+      .withColumn("_from", col("parent"))
+      .withColumn("_to", col("id"))
+      .drop("parents", "parent", "id")
   }
 
   def apply()(implicit context: ETLSessionContext) = {
     implicit val ss = context.sparkSession
 
-    val dfName = "edges"
+    val dfName = "disease_edges"
     val common = context.configuration.common
 
     val dEdges = diseaseEdges(Disease.compute())
 
+    val outputDFs = Seq(dEdges)
     val outputs = Seq(dfName)
+
     val outputConfs = outputs
       .map(
-        name =>
-          name -> IOResourceConfig(common.outputFormat, common.output + s"/$name")
-      ).toMap
+        name => name -> IOResourceConfig(common.outputFormat, common.output + s"/$name")
+      )
+      .toMap
 
-    val outputDFs = (outputs zip Seq(dEdges)).toMap
-    SparkHelpers.writeTo(outputConfs, outputDFs)
+    val outputDFConfs = (outputs zip outputDFs).toMap
+    SparkHelpers.writeTo(outputConfs, outputDFConfs)
   }
 }
