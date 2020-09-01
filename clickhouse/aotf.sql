c08ca2b2de7e9b6894d64c49e5fcbfdf4ceab8c4
Miguel C <mkarmona@gmail.com>
2020-09-01 10:10:48 +0100

    wip: rename few fields needed

--- clickhouse/aotf.sql
+++ clickhouse/aotf.sql
@@ -65,3 +65,51 @@ as select
     target_name,
     target_symbol 
 from ot.aotf_indirect_log;
+
+create table if not exists ot.associations_otf_left
+engine = MergeTree()
+order by (A, B, datasource_id)
+primary key (A)
+as select
+    row_id,
+    A,
+    B,
+    datatype_id,
+    datasource_id,
+    row_score,
+    A_data,
+    B_data 
+from (select 
+        row_id,
+        left_id as A,
+        right_id as B,
+        datatype_id,
+        datasource_id,
+        row_score,
+        left_data as A_data,
+        right_data as B_data
+    from ot.associations_otf_log);
+
+create table if not exists ot.associations_otf_right
+engine = MergeTree()
+order by (A, B, datasource_id)
+primary key (A)
+as select
+    row_id,
+    A,
+    B,
+    datatype_id,
+    datasource_id,
+    row_score,
+    A_data,
+    B_data
+from (select 
+        row_id,
+        left_id as B,
+        right_id as A,
+        datatype_id,
+        datasource_id,
+        row_score,
+        left_data as B_data,
+        right_data as A_data
+    from ot.associations_otf_log);
---
+++ src/main/scala/io/opentargets/etl/backend/AssociationOTF.scala
@@ -0,0 +1,65 @@
+package io.opentargets.etl.backend
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.{pow => powCol}
+import better.files.Dsl._
+import better.files._
+import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigRenderOptions}
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import io.opentargets.etl.backend.SparkHelpers.IOResourceConfig
+import org.apache.spark.sql.expressions._
+
+import scala.math.pow
+
+object AssociationOTF extends LazyLogging {
+  def prepareEvidences(expandOntology: Boolean = false)(implicit context: ETLSessionContext): Map[String, DataFrame] = {
+    implicit val ss = context.sparkSession
+    import ss.implicits._
+    import AssociationHelpers._
+
+    val commonSec = context.configuration.common
+    val associationsSec = context.configuration.associations
+
+    val mappedInputs = Map(
+      "evidences" -> IOResourceConfig(
+        commonSec.inputs.evidence.format,
+        commonSec.inputs.evidence.path
+      )
+    )
+    val dfs = SparkHelpers.readFrom(mappedInputs)
+
+    val evidenceColumns = Seq(
+      "id as row_id",
+      "disease.id as A",
+      "concat(disease.id, ' ',disease.name) as A_data",
+      "target.id as B",
+      "concat(target.id, ' ', target.gene_info.name, ' ', target.gene_info.symbol) as B_data",
+      "sourceID as datasource_id",
+      "`type` as datatype_id",
+      "scores.association_score as row_score",
+      "unique_association_fields.*"
+    )
+
+
+    Map("evidences_aotf" -> dfs("evidences")
+      .selectExpr(evidenceColumns:_*)
+      .where($"row_score" > 0D)
+      .repartition()
+    )
+  }
+
+  def apply()(implicit context: ETLSessionContext) = {
+    implicit val ss = context.sparkSession
+    val commonSec = context.configuration.common
+
+    val clickhouseEvidences = prepareEvidences()
+
+    val outputs = clickhouseEvidences.keys map (name =>
+      name -> IOResourceConfig(commonSec.outputFormat, commonSec.output + s"/$name"))
+
+    SparkHelpers.writeTo(outputs.toMap, clickhouseEvidences)
+  }
+}
