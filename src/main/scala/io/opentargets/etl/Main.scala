e0de823582f45e654868bbe4df45d3fb441fdbb4
Miguel C <mkarmona@gmail.com>
2020-09-17 11:43:01 +0100

    refactor: minor cleannings

--- src/main/scala/io/opentargets/etl/Main.scala
+++ src/main/scala/io/opentargets/etl/Main.scala
@@ -7,7 +7,7 @@ import io.opentargets.etl.backend._
 
 object ETL extends LazyLogging {
 
-  def applySingleStep(step: String)(implicit context: ETLSessionContext) = {
+  def applySingleStep(step: String)(implicit context: ETLSessionContext): Unit = {
     step match {
       case "search" =>
         logger.info("run step search")
@@ -70,11 +70,11 @@ object ETL extends LazyLogging {
     logger.info(s"finished to run step ($step)")
   }
 
-  def apply(steps: Seq[String]) = {
+  def apply(steps: Seq[String]): Unit = {
 
     ETLSessionContext() match {
       case Right(otContext) =>
-        implicit val ctxt = otContext
+        implicit val ctxt: ETLSessionContext = otContext
 
         logger.debug(ctxt.configuration.toString)
 
@@ -88,10 +88,9 @@ object ETL extends LazyLogging {
         logger.info(s"valid steps to execute: ${knownSteps.toString}")
         logger.warn(s"invalid steps to skip: ${unknownSteps.toString}")
 
-        knownSteps.foreach {
-          case step =>
-            logger.debug(s"step to run: '${step}'")
-            ETL.applySingleStep(step)
+        knownSteps.foreach { step =>
+          logger.debug(s"step to run: '$step'")
+          ETL.applySingleStep(step)
         }
 
       case Left(ex) => logger.error(ex.prettyPrint())
