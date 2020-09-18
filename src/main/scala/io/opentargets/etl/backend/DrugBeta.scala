1b7b0f611a16386b022f8151fb038aae6b8d0c71
Jarrod Baker <jarrod.bakeriii@gmail.com>
2020-09-18 16:56:25 +0100

    wip: adding cross-reference processing

--- src/main/scala/io/opentargets/etl/backend/DrugBeta.scala
+++ src/main/scala/io/opentargets/etl/backend/DrugBeta.scala
@@ -2,9 +2,21 @@ package io.opentargets.etl.backend
 
 import com.typesafe.scalalogging.LazyLogging
 import io.opentargets.etl.backend.SparkHelpers.IOResourceConfig
-
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.{array, arrays_zip, col, collect_set, explode, split, upper, when}
+import org.apache.spark.sql.functions.{
+  array,
+  array_sort,
+  arrays_zip,
+  col,
+  collect_list,
+  collect_set,
+  explode,
+  map_from_arrays,
+  split,
+  udf,
+  upper,
+  when
+}
 
 /**
   * This step will eventually replace the existing Drug step.
@@ -31,11 +43,11 @@ object MoleculeHelper extends LazyLogging {
     (results._1.map(_._1), results._2.map(_._1))
   }
 
-  implicit class MoleculeHelpers(dataframe: DataFrame)(implicit ss: SparkSession){
+  implicit class MoleculeHelpers(dataframe: DataFrame)(implicit ss: SparkSession) {
     import ss.implicits._
 
     /**
-      * Method to group synonyms into trade names and others.
+      * Method to group synonyms into sorted sets of trade names and others synonyms.
       * @param preProcessedMolecules df prepared with moleculePreprocess method
       * @return dataframe of `id: String, trade_name: Set[String], synonym: Set[String]`
       */
@@ -45,19 +57,58 @@ object MoleculeHelper extends LazyLogging {
         .withColumn("syn_type", upper($"col.synonym_type"))
         .withColumn("synonym", $"col.mol_synonyms")
 
-      val tradeName = synonyms.filter($"syn_type" === "TRADE_NAME")
+      val tradeName = synonyms
+        .filter($"syn_type" === "TRADE_NAME")
         .groupBy($"id")
         .agg(collect_set($"synonym").alias("trade_name"))
-      val synonym = synonyms.filter($"syn_type" =!= "TRADE_NAME")
+      val synonym = synonyms
+        .filter($"syn_type" =!= "TRADE_NAME")
         .groupBy($"id")
         .agg(collect_set($"synonym").alias("synonym"))
 
       val full = tradeName
         .join(synonym, Seq("id"), "fullouter")
-        .withColumn("synonym", when($"synonym".isNull, array().cast("array<string>")).otherwise($"synonym"))
-        .withColumn("trade_name", when($"trade_name".isNull, array().cast("array<string>")).otherwise($"trade_name"))
-
+        // cast nulls to empty arrays
+        .withColumn("synonym",
+                    when($"synonym".isNull, array().cast("array<string>")).otherwise($"synonym"))
+        .withColumn(
+          "trade_name",
+          when($"trade_name".isNull, array().cast("array<string>")).otherwise($"trade_name"))
+        // sort lists
+        .withColumn("synonym", array_sort($"synonym"))
+        .withColumn("trade_name", array_sort($"trade_name"))
       full
+    }
+
+    /**
+      * Method to group cross references for each molecule id. Source ids are grouped according to source.
+      * @param preProcessedMolecules df prepared with moleculePreprocess methods
+      * @return dataframe of `id: String, source: Map(src: String -> ids: Array[String])`
+      */
+    def processMoleculeCrossReferences(preProcessedMolecules: DataFrame): DataFrame = {
+      def fun(refSrcAndId: Seq[Seq[String]]): Map[String, Seq[String]] = {
+        // (src, id)
+        val i: Seq[(String, String)] =
+          refSrcAndId.filter(_.size == 2).map(s => (s.head, s(1)))
+
+        i.groupBy(k => k._1).map(v => (v._1, v._2.map(_._2)))
+      }
+
+      val createReferenceMap = udf(fun _)
+
+      val xr = preProcessedMolecules
+        .select($"id",
+                explode(arrays_zip($"cross_references.xref_id".as("ref_id"),
+                                   $"cross_references.xref_src".as("ref_source"))).as("sources"))
+        .withColumn("ref_id", $"sources.xref_id")
+        .withColumn("ref_src", $"sources.xref_src")
+        .withColumn("refs", array($"ref_src", $"ref_id").as("refs"))
+        .drop("sources", "ref_id", "ref_src")
+
+        xr.groupBy("id")
+        .agg(collect_list($"refs").as("ref1"))
+        .withColumn("xref", createReferenceMap($"ref1"))
+        .drop("refs", "ref1")
     }
   }
 
@@ -74,13 +125,13 @@ object DrugBeta extends LazyLogging {
     logger.info("Loading raw inputs for Drug beta step.")
     val mappedInputs = Map(
       "indication" -> IOResourceConfig(common.inputs.drugChemblIndication.format,
-        common.inputs.drugChemblIndication.path),
+                                       common.inputs.drugChemblIndication.path),
       "mechanism" -> IOResourceConfig(common.inputs.drugChemblMechanism.format,
-        common.inputs.drugChemblMechanism.path),
+                                      common.inputs.drugChemblMechanism.path),
       "molecule" -> IOResourceConfig(common.inputs.drugChemblMolecule.format,
-        common.inputs.drugChemblMolecule.path),
+                                     common.inputs.drugChemblMolecule.path),
       "target" -> IOResourceConfig(common.inputs.drugChemblTarget.format,
-        common.inputs.drugChemblTarget.path)
+                                   common.inputs.drugChemblTarget.path)
     )
 
     val inputDataFrames = SparkHelpers.readFrom(mappedInputs)
@@ -91,22 +142,6 @@ object DrugBeta extends LazyLogging {
     val targetDf: DataFrame = inputDataFrames("target")
 
     logger.info("Raw inputs for Drug beta loaded.")
-
-    def processMoleculeSynonyms(dataFrame: DataFrame): DataFrame = {
-      val synonyms: DataFrame = dataFrame
-        .select($"id", explode($"syns"))
-        .withColumn("syn_type", upper($"col.synonym_type"))
-        .withColumn("synonym", $"col.mol_synonyms")
-
-      val tradeName = synonyms.filter($"syn_type" === "TRADE_NAME")
-        .groupBy($"id")
-        .agg(collect_set($"synonym").alias("trade_name"))
-      val synonym = synonyms.filter($"syn_type" =!= "TRADE_NAME")
-        .groupBy($"id")
-        .agg(collect_set($"synonym").alias("synonym"))
-
-      tradeName.join(synonym, "id")
-    }
 
     /**
       *
