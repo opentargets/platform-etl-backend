bafa966f02e7dfe257b3c96db4f852daf078d649
Miguel C <mkarmona@gmail.com>
2020-10-05 14:43:36 +0100

    refactor: es scripts

--- elasticsearch/load_evidences_aotf.sh
+++ elasticsearch/load_evidences_aotf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index cancerbiomarker --type cancerbiomarker json --json-lines -
 
-export INDEX_SETTINGS="index_settings.json"
+export INDEX_SETTINGS="index_settings_evidences_aotf.json"
 export RELEASE=''
-export INDEX_NAME="cancerbiomarker"
-export TYPE_FIELD="cancerbiomarker"
-export INPUT="../out/cancerBiomarkers"
+export INDEX_NAME="evidences_aotf"
+export TYPE_FIELD="evidences_aotf"
+export INPUT="../out/evidences_aotf"
 #export INPUT="gs://ot-snapshots/etl/latest/cancerBiomarkers"
 export ES="http://localhost:9200"
 
