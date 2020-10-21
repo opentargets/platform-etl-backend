f6555621326d82a1adf2bd6f0e2399154ca0ddcd
Jarrod Baker <jarrod.bakeriii@gmail.com>
2020-10-21 13:39:36 +0100

    Refactor Indication to use Object rather than Class.

--- src/main/scala/io/opentargets/etl/backend/drug_beta/DrugBeta.scala
+++ src/main/scala/io/opentargets/etl/backend/drug_beta/DrugBeta.scala
@@ -60,11 +60,10 @@ object DrugBeta extends Serializable with LazyLogging {
     logger.info("Raw inputs for Drug beta loaded.")
     logger.info("Processing Drug beta transformations.")
     val molecule = new Molecule(moleculeDf, drugbankData)
-    val indications = new Indication(indicationDf, efoDf)
     val mechanismOfAction = new MechanismOfAction(mechanismDf, targetDf, geneDf)
-
     val moleculeProcessedDf = molecule.processMolecules
-    val indicationProcessedDf = indications.processIndications
+
+    val indicationProcessedDf = Indication(indicationDf, efoDf)
     val mechanismOfActionProcessedDf = mechanismOfAction.processMechanismOfAction
     val targetsAndDiseasesDf =
       DrugCommon.getUniqTargetsAndDiseasesPerDrugId(evidenceDf).withColumnRenamed("drug_id", "id")
