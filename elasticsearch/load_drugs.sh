1b0257eef961ced1e71ef681aeecbed044f59860
Jarrod Baker <jarrod.bakeriii@gmail.com>
2021-01-22 10:35:00 +0000

    Update load_drug.sh to have no id field.

--- elasticsearch/load_drugs.sh
+++ elasticsearch/load_drugs.sh
@@ -6,7 +6,6 @@ subindexes=('drug' 'mechanism_of_action' 'indication')
 for si in "${subindexes[@]}"; do
   export INDEX_NAME=$si
   export INPUT="${PREFIX}/drugs/$si"
-  export ID="id"
 
   ./load_jsons.sh
 done
