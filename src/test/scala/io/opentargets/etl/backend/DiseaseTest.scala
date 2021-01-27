85dbe35d62a0255a37a44df57edae85283b8a8e7
Cinzia Malangone <cinzia.malangone@gmail.com>
2021-01-27 14:41:59 +0000

    draft

---
+++ src/test/scala/io/opentargets/etl/backend/DiseaseTest.scala
@@ -0,0 +1,34 @@
+package io.opentargets.etl.backend.DiseaseTest
+
+import io.opentargets.etl.backend.EtlSparkUnitTest
+import io.opentargets.etl.backend.Disease
+import io.opentargets.etl.backend.spark.Helpers
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{col, explode}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+object DiseaseTest {
+
+  def efoDf(implicit sparkSession: SparkSession): DataFrame =
+    sparkSession.read.json(this.getClass.getResource("/ontology-efo_sample.jsonl.gz").getPath)
+}
+
+class DiseaseTest extends EtlSparkUnitTest {
+
+  import sparkSession.implicits._
+
+  "Processing EFO ontology input file" should "return a dataframe with a specific list of attributes" in {
+    // given
+    val inputDF: DataFrame = DiseaseTest.efoDf(sparkSession)
+    val expectedColumns = Set("id", "name", "description")
+    // when
+    val results: DataFrame = Disease.setIdAndSelectFromDiseases(inputDF)
+
+    results.columns.foreach(x => println(x))
+    expectedColumns.foreach(y => println(y))
+
+    // then
+    assert(results.columns.forall(expectedColumns.contains))
+  }
+
+}
