c0de1d8933b6e10efc41fb2c77478aba14c21bff
Cinzia <cinzia.malangone@gmail.com>
2021-03-23 16:22:32 +0000

    Fixed script

--- elasticsearch/load_evidences_aotf.sh
+++ elasticsearch/load_evidences_aotf.sh
@@ -2,6 +2,6 @@
 #cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index cancerbiomarker --type cancerbiomarker json --json-lines -
 
 export INDEX_NAME="evidences_aotf"
-export INPUT="${PREFIX}/aotf/elasticsearch"
+export INPUT="${PREFIX}/AOTFElasticsearch"
 
 ./load_jsons.sh
--- elasticsearch/load_known_drugs.sh
+++ elasticsearch/load_known_drugs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export INDEX_SETTINGS="index_settings_search_known_drugs.json"
 export INDEX_NAME="known_drugs"
-export INPUT="${PREFIX}/knownDrugs"
+export INPUT="${PREFIX}/knownDrugsAggregated"
 
 bash ./load_jsons.sh
