063422b348d275c7c0b6419d5cce57b5caecf320
Cinzia <cinzia.malangone@gmail.com>
2021-03-23 14:26:44 +0000

    Fix load script

--- elasticsearch/load_drugs.sh
+++ elasticsearch/load_drugs.sh
@@ -1,10 +1,10 @@
 #cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index drugs --type drug --id-field id json --json-lines -
 
-subindexes=('drug' 'mechanism_of_action' 'indication' 'drug_warnings')
+subindexes=('molecule' 'mechanismOfAction' 'indication' 'drugWarnings')
 
 for si in "${subindexes[@]}"; do
   export INDEX_NAME=$si
-  export INPUT="${PREFIX}/drugs/$si"
+  export INPUT="${PREFIX}/$si"
 
   ./load_jsons.sh
 done
