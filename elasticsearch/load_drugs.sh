64090ca4cd0e2f9844ae4032ea1a0780e9f010a1
Cinzia <cinzia.malangone@gmail.com>
2021-03-24 09:07:34 +0000

    Drug index

--- elasticsearch/load_drugs.sh
+++ elasticsearch/load_drugs.sh
@@ -1,7 +1,7 @@
 #cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index drugs --type drug --id-field id json --json-lines -
 
 subindexes=('molecule' 'mechanismOfAction' 'indication' 'drugWarnings')
-subindexesnames=('molecule' 'mechanism_of_action' 'indication' 'drug_warnings')
+subindexesnames=('drug' 'mechanism_of_action' 'indication' 'drug_warnings')
 
 len=${#subindexes[@]}
 for (( i=0; i<$len; i++ )); do
