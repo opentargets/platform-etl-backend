35f8b89fbb86cdee7aeb3996177b13d190093973
opentargets-admin <administrator@opentargets.org>
2021-04-14 21:33:49 +0100

    reactome using graph object

--- src/main/scala/io/opentargets/etl/backend/Reactome.scala
+++ src/main/scala/io/opentargets/etl/backend/Reactome.scala
@@ -1,83 +1,19 @@
 package io.opentargets.etl.backend
 
 import com.typesafe.scalalogging.LazyLogging
+import io.opentargets.etl.backend.graph.GraphNode
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import io.opentargets.etl.backend.spark.Helpers
 import io.opentargets.etl.backend.spark.Helpers.{IOResource, IOResourceConfig, IOResources}
-import scala.collection.convert.ImplicitConversions._
-import org.jgrapht._
-import org.jgrapht.generate._
-import org.jgrapht.traverse._
-import org.jgrapht.graph._
-import org.jgrapht.Graphs._
-import org.jgrapht.util._
-import org.jgrapht.alg.shortestpath._
 
 // This is option/step reactome in the config file
 object Reactome extends LazyLogging {
-
-  type DAGT = DirectedAcyclicGraph[String, DefaultEdge]
-  case class GraphNodeDocument(id: String,
-                               label: String,
-                               ancestors: Seq[String],
-                               descendants: Seq[String],
-                               children: Seq[String],
-                               parents: Seq[String],
-                               path: Seq[Seq[String]])
 
   def cleanPathways(df: DataFrame): DataFrame =
     df.filter(col("_c2") === "Homo sapiens")
       .drop("_c2")
       .toDF("id", "name")
-
-  def makeGraph(pathways: DataFrame, relations: DataFrame): DAGT = {
-    val vertices = pathways
-
-    val edges = relations
-      .toDF("src", "dst")
-
-    val jg = new org.jgrapht.graph.DirectedAcyclicGraph[String, DefaultEdge](classOf[DefaultEdge])
-
-    vertices.collect.foreach(r => jg.addVertex(r.getAs[String]("id")))
-    edges.collect.foreach(r => {
-      try {
-        jg.addEdge(r.getAs[String]("src"), r.getAs[String]("dst"))
-      } catch {
-        case _ =>
-      }
-    })
-
-    jg
-
-  }
-
-  def processGraph(vertices: DataFrame, graph: DAGT)(
-      implicit context: ETLSessionContext): DataFrame = {
-    import context.sparkSession.implicits._
-
-    logger.debug("compute the graph")
-    val topV = graph.vertexSet.filter(p => graph.inDegreeOf(p) == 0).toSeq
-    val adp = new AllDirectedPaths(graph)
-
-    val V = vertices.collectAsList.map { r =>
-      val id = r.getString(0)
-      val label = r.getString(1)
-      val paths = adp
-        .getAllPaths(topV.toSet, Set(id), true, null)
-        .toSeq
-        .map(e => e.getVertexList.toSeq)
-      GraphNodeDocument(id,
-                        label,
-                        graph.getAncestors(id).toSeq,
-                        graph.getDescendants(id).toSeq,
-                        successorListOf(graph, id),
-                        predecessorListOf(graph, id),
-                        paths)
-    }
-
-    V.toDF
-  }
 
   def apply()(implicit context: ETLSessionContext): IOResources = {
     implicit val ss: SparkSession = context.sparkSession
@@ -91,8 +27,9 @@ object Reactome extends LazyLogging {
 
     val reactomeIs = Helpers.readFrom(mappedInputs)
     val pathways = reactomeIs("pathways").data.transform(cleanPathways)
-    val g = makeGraph(pathways, reactomeIs("relations").data)
-    val index = processGraph(pathways, g)
+    val edges = reactomeIs("relations").data.toDF("src", "dst")
+
+    val index = GraphNode(pathways, edges)
 
     logger.info("compute reactome dataset")
     val outputs = Map(
