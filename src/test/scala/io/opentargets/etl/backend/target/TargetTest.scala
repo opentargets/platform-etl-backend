fdc33eb530c7c30ac0584ac1472364d4f85a1b29
Jarrod Baker <jarrod.bakeriii@gmail.com>
2021-05-19 10:24:53 +0100

    Delete unused test input file and test.

--- src/test/scala/io/opentargets/etl/backend/target/TargetTest.scala
+++ src/test/scala/io/opentargets/etl/backend/target/TargetTest.scala
@@ -1,28 +1,12 @@
 package io.opentargets.etl.backend.target
 
-import io.opentargets.etl.backend.Configuration.OTConfig
-import io.opentargets.etl.backend.{Configuration, EtlSparkUnitTest}
-import io.opentargets.etl.backend.spark.{IOResource, IOResourceConfig}
-import io.opentargets.etl.backend.target.UniprotTest.uniprotDataPath
+import io.opentargets.etl.backend.EtlSparkUnitTest
+
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import pureconfig.ConfigReader
 
 object TargetTest {}
 
 class TargetTest extends EtlSparkUnitTest {
-  "Target" should "read in raw Uniprot data and preprocessing complete" in {
-//    // given
-//    val conf: ConfigReader.Result[OTConfig] = Configuration.config
-//    val getUniprotDataFrame = PrivateMethod[Map[String, IOResource]]('getMappedInputs)
-    //    // when
-    //    val results = io.opentargets.etl.backend.target.Target invokePrivate getUniprotDataFrame(
-    //      conf.right.get.target,
-    //      sparkSession)
-    //    // then
-    //    val uniprot = results.get("uniprot")
-    //    uniprot shouldBe defined
-    //    uniprot.get.data.count() must be(10)
-  }
 
   "ProteinIds" should "not have duplicate accession entries and be returned in sorted order" in {
     // given
