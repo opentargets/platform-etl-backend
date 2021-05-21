dc8023b136d085570b5818115b0eee982b71ab0e
Jarrod Baker <jarrod.bakeriii@gmail.com>
2021-05-21 14:33:12 +0100

    Add ETLSessionContext to EtlSparkUnitTest
    Some methods require that we pass in an instances of ETLSessionContext. This is created with a custom Spark session for testing and uses the default configuration. If the configTest passes, there should be no problem loading an ETLSessionContext.
    
    It's possible to override the configuration by adding a custom application.conf file in the test/resources directory or overriding the system properties.

--- src/test/scala/io/opentargets/etl/backend/EtlSparkUnitTest.scala
+++ src/test/scala/io/opentargets/etl/backend/EtlSparkUnitTest.scala
@@ -1,11 +1,24 @@
 package io.opentargets.etl.backend
 
+import io.opentargets.etl.backend.ETLSessionContext.progName
+import io.opentargets.etl.backend.spark.Helpers.getOrCreateSparkSession
 import org.scalatest.PrivateMethodTester
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.must.Matchers
+import pureconfig.error.ConfigReaderFailures
 
 trait EtlSparkUnitTest
-  extends AnyFlatSpecLike
+    extends AnyFlatSpecLike
     with PrivateMethodTester
     with SparkSessionSetup
-    with Matchers
+    with Matchers {
+
+  private val ctxE: Either[ConfigReaderFailures, ETLSessionContext] = for {
+    config <- Configuration.config
+  } yield ETLSessionContext(config, sparkSession)
+
+  val ctx = ctxE match {
+    case Left(value)  => sys.exit(1)
+    case Right(value) => value
+  }
+}
