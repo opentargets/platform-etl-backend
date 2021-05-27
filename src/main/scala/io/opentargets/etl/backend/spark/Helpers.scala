af87eeeb9b7c17e5c9f4bab2bd52a60d97134269
Miguel C <mkarmona@gmail.com>
2021-05-27 13:15:25 +0100

    fix: random needs to be imported to work

--- src/main/scala/io/opentargets/etl/backend/spark/Helpers.scala
+++ src/main/scala/io/opentargets/etl/backend/spark/Helpers.scala
@@ -20,6 +20,7 @@ import org.apache.spark.sql.functions.{
 import org.apache.spark.sql.types.{ArrayType, DataType, StructField, StructType}
 
 import scala.language.postfixOps
+import scala.util.Random
 
 object Helpers extends LazyLogging {
 
