2bcbbf1d7b2aa814d857de5bb905a8f3f8e19ffe
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-07-21 13:03:32 +0100

    openfda writers

---
+++ src/main/scala/io/opentargets/etl/backend/openfda/utils/Writers.scala
@@ -0,0 +1,53 @@
+package io.opentargets.etl.backend.openfda.utils
+
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object Writers extends LazyLogging {
+
+  def writeMonteCarloResults(results: DataFrame, outputPath: String, extension: String)(
+      implicit sparkSession: SparkSession): Unit = {
+
+    extension match {
+      case "csv" =>
+        // write to one single compressed file
+        logger.info("Writing monte carlo results as csv compressed output...")
+        val fileName = s"$outputPath/agg_critval_drug_csv/"
+        results
+          .coalesce(1)
+          .write
+          .option("compression", "gzip")
+          .option("header", "true")
+          .csv(s"$outputPath")
+
+      case "json" =>
+        logger.info("Writing monte carlo results as json output...")
+        results.write
+          .json(s"$outputPath")
+      case "parquet" =>
+        logger.info("Writing monte carlo results as parquet output...")
+        results.write
+          .format("parquet")
+          .save(s"$outputPath")
+
+      case err: String => logger.error(s"Unrecognised output format $err")
+    }
+
+  }
+
+  def writeFdaResults(results: DataFrame, outputPath: String, extension: String): Unit = {
+    logger.info("Writing results of fda data transformation aggregated by Chembl.")
+    extension match {
+      case "json" =>
+        logger.info("format json")
+        results.write.
+           format(extension).save(s"${outputPath}Unfiltered")
+      case "parquet" =>
+        logger.info("format paquet")
+        results.write.
+          format(extension).save(s"${outputPath}Unfiltered")
+
+      case err: String => logger.error(s"Unrecognised output format $err")
+    }
+  }
+}
