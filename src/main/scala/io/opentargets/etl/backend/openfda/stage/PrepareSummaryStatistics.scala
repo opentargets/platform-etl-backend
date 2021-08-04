1ee8128fe54c615e921622622c61ebe07595a0cd
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-08-04 14:00:12 +0100

    openfda etl, prepare summary statistics

---
+++ src/main/scala/io/opentargets/etl/backend/openfda/stage/PrepareSummaryStatistics.scala
@@ -0,0 +1,41 @@
+package io.opentargets.etl.backend.openfda.stage
+
+import io.opentargets.etl.backend.ETLSessionContext
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{approx_count_distinct, col}
+
+/*
+    Project     : io-opentargets-etl-backend
+    Timestamp   : 2021-08-04T13:57
+    Author      : Manuel Bernal Llinares <mbdebian@gmail.com>
+*/
+
+object PrepareSummaryStatistics {
+  def apply(fdaData: DataFrame)(implicit context: ETLSessionContext) = {
+
+    // Define the partition windows
+    val wAdverses = Window.partitionBy(col("reaction_reactionmeddrapt"))
+    val wDrugs = Window.partitionBy(col("chembl_id"))
+    val wAdverseDrugComb =
+      Window.partitionBy(col("chembl_id"), col("reaction_reactionmeddrapt"))
+
+    // and we will need this processed data later on
+    val groupedDf = fdaData
+      .withColumn("uniq_report_ids_by_reaction", // how many reports mention that reaction
+        approx_count_distinct(col("safetyreportid")).over(wAdverses))
+      .withColumn("uniq_report_ids_by_drug", // how many reports mention that drug
+        approx_count_distinct(col("safetyreportid")).over(wDrugs))
+      .withColumn("uniq_report_ids", // how many mentions of drug-reaction pair
+        approx_count_distinct(col("safetyreportid")).over(wAdverseDrugComb))
+      .select(
+        "safetyreportid",
+        "chembl_id",
+        "reaction_reactionmeddrapt",
+        "uniq_report_ids_by_reaction",
+        "uniq_report_ids_by_drug",
+        "uniq_report_ids"
+      )
+    groupedDf
+  }
+}
