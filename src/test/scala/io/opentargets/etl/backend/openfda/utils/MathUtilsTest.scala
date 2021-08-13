d91e7543cf821b554527bfb461b508fcea7485cd
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-08-13 13:30:31 +0100

    first unit test mechanism testing

---
+++ src/test/scala/io/opentargets/etl/backend/openfda/utils/MathUtilsTest.scala
@@ -0,0 +1,12 @@
+package io.opentargets.etl.backend.openfda.utils
+
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class MathUtilsTest extends AnyFlatSpecLike with Matchers {
+  import breeze.linalg._
+
+  "My first test" should "work with no problem" in {
+    // The code you don't write, is code that works
+  }
+}
