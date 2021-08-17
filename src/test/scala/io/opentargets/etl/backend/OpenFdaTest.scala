508631e11b3c13b4c43452596a62504c6159f6fb
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-08-17 11:37:45 +0100

    openfda etl, unit testing sample data load

--- src/test/scala/io/opentargets/etl/backend/OpenFdaTest.scala
+++ src/test/scala/io/opentargets/etl/backend/OpenFdaTest.scala
@@ -1,9 +1,35 @@
 package io.opentargets.etl.backend
 
+import io.opentargets.etl.backend.spark.{IOResourceConfig, IOResourceConfigOption}
+import io.opentargets.etl.backend.spark.IoHelpers
+import io.opentargets.etl.backend.spark.IoHelpers.IOResourceConfigurations
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
-class OpenFdaTest
-  extends AnyWordSpecLike
-  with SparkSessionSetup {
-  // TODO
+class OpenFdaTest extends AnyWordSpecLike with Matchers with SparkSessionSetup {
+
+  "The OpenFDA FAERS ETL Stage" should {
+    // Load testing data
+    val sourceData =
+      Map(
+        DrugData() -> IOResourceConfig("json",
+                                       this.getClass.getResource("/drug_test.json").getPath),
+        Blacklisting() -> IOResourceConfig(
+          "csv",
+          this.getClass.getResource("/blacklisted_events.txt").getPath,
+          Option(
+            Seq(
+              IOResourceConfigOption("sep", "\\t"),
+              IOResourceConfigOption("ignoreLeadingWhiteSpace", "true"),
+              IOResourceConfigOption("ignoreTrailingWhiteSpace", "true")
+            ))
+        ),
+        FdaData() -> IOResourceConfig(
+          "json",
+          this.getClass.getResource("/adverseEventSample.jsonl").getPath)
+      )
+    // Read the files
+    val dfsData = IoHelpers.readFrom(sourceData)
+  }
+
 }
