581077645053d1bc130f22ea9da772c85eebe411
Cinzia <cinzia.malangone@gmail.com>
2021-08-19 15:06:25 +0100

    Save csv

---
+++ src/main/scala/io/opentargets/etl/backend/EBISearch.scala
@@ -0,0 +1,72 @@
+package io.opentargets.etl.backend
+
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import io.opentargets.etl.backend.spark.{IOResource, IOResourceConfig, IoHelpers}
+import io.opentargets.etl.backend.spark.IoHelpers
+import io.opentargets.etl.backend.spark.IoHelpers.IOResources
+import org.apache.spark.sql.SparkSession
+
+object EBISearch extends LazyLogging {
+
+  private def exportToCSV(dataToSave: DataFrame, path: String) = {
+
+    dataToSave
+      .repartition(1)
+      .write
+      .option("header", "true")
+      .option("sep", ",")
+      .mode("overwrite")
+      .csv(path)
+  }
+
+  def generateDatasets(resources: IoHelpers.IOResources): Map[String, DataFrame] = {
+
+    val diseases = resources("diseases").data.withColumnRenamed("id", "diseaseId")
+    val targets = resources("targets").data.withColumnRenamed("id", "targetId")
+    val associationsDirectOverall = resources("associationDirectOverall").data
+    val evidence = resources("evidence").data
+
+    val datasetAssociations = associationsDirectOverall
+      .join(targets, Seq("targetId"), "inner")
+      .join(diseases, Seq("diseaseId"), "inner")
+      .select("targetId", "diseaseId", "approvedSymbol", "name", "score")
+
+    val datasetEvidence = evidence
+      .join(targets, Seq("targetId"), "inner")
+      .join(diseases, Seq("diseaseId"), "inner")
+      .select("targetId", "diseaseId", "approvedSymbol", "name", "score")
+
+    Map(
+      "ebisearchAssociations" -> datasetAssociations,
+      "ebisearchEvidence" -> datasetEvidence
+    )
+
+  }
+  def apply()(implicit context: ETLSessionContext) = {
+    implicit val ss: SparkSession = context.sparkSession
+
+    logger.info("Generate EBI Search dataset")
+
+    val EBIConfiguration = context.configuration.ebisearch
+
+    logger.info("Loading raw inputs for Base Expression step.")
+
+    val mappedInputs = Map(
+      "diseases" -> EBIConfiguration.diseaseEtl,
+      "targets" -> EBIConfiguration.targetEtl,
+      "evidence" -> EBIConfiguration.evidenceETL,
+      "associationDirectOverall" -> EBIConfiguration.associationETL,
+    )
+
+    val inputDataFrames = IoHelpers.readFrom(mappedInputs)
+    val dataToSave = generateDatasets(inputDataFrames)
+
+    logger.info(s"write to ${context.configuration.common.output}/ebisearch")
+    exportToCSV(dataToSave("ebisearchEvidence"), EBIConfiguration.outputs.ebisearchEvidence.path)
+    exportToCSV(dataToSave("ebisearchAssociations"),
+                EBIConfiguration.outputs.ebisearchAssociations.path)
+
+  }
+}
---
+++ src/test/scala/io/opentargets/etl/backend/EBISearchTest.scala
@@ -0,0 +1,83 @@
+package io.opentargets.etl.backend
+
+import io.opentargets.etl.backend.Configuration.OTConfig
+import io.opentargets.etl.backend.spark.{
+  Helpers,
+  IOResource,
+  IOResourceConfig,
+  IOResourceConfigOption,
+  IoHelpers
+}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import io.opentargets.etl.backend.spark.{IOResource, IOResourceConfig, IoHelpers}
+import io.opentargets.etl.backend.spark.IoHelpers
+import io.opentargets.etl.backend.spark.IoHelpers.IOResources
+import pureconfig.ConfigReader
+
+object EBISearchTest {
+
+  def setupResources(implicit sparkSession: SparkSession): IOResources = {
+
+    val inputTargets = Seq(
+      ("ENSG00000157764", "BRAF"),
+      ("ENSG00000171862", "PTEN"),
+      ("ENSG00000139618", "BRCA2"),
+      ("ENSG00000142192", "APP")
+    )
+    val targets = sparkSession.createDataFrame(inputTargets).toDF("id", "approvedSymbol")
+
+    val inputDiseases = Seq(
+      ("EFO_0003767", "inflammatory bowel disease"),
+      ("EFO_0000729", "ulcerative colitis"),
+      ("EFO_0000692", "schizophrenia")
+    )
+    val diseases = sparkSession.createDataFrame(inputDiseases).toDF("id", "name")
+
+    val inputEvidence = Seq(
+      ("ENSG00000157764", "EFO_0003767", 0.78778),
+      ("ENSG00000139618", "EFO_0000692", 0.98987),
+      ("ENSG00000171862", "EFO_0000729", 0.4323)
+    )
+    val evidence =
+      sparkSession.createDataFrame(inputEvidence).toDF("targetId", "diseaseId", "score")
+
+    val inputAssociations = Seq(
+      ("ENSG00000171862", "EFO_0003767", 0.8737),
+      ("ENSG00000139618", "EFO_0000692", 0.5555),
+      ("ENSG00000171862", "EFO_0000729", 0.3232)
+    )
+    val associations =
+      sparkSession.createDataFrame(inputAssociations).toDF("targetId", "diseaseId", "score")
+
+    val config = IOResourceConfig("csv", "")
+    val allResources: IOResources =
+      Map(
+        "targets" -> IOResource(targets, config),
+        "diseases" -> IOResource(diseases, config),
+        "evidence" -> IOResource(evidence, config),
+        "associationDirectOverall" -> IOResource(associations, config)
+      )
+    allResources
+  }
+}
+
+class EBISearchTest extends EtlSparkUnitTest {
+  import sparkSession.implicits._
+
+  "Processing Diseases,Target and Evidence" should "return a dataframe with a specific list of attributes" in {
+    // given
+    val resources: IOResources = EBISearchTest.setupResources(sparkSession)
+    val expectedColumns = Set("diseaseId", "targetId", "score", "approvedSymbol", "name")
+    // when
+    val results = EBISearch.generateDatasets(resources)
+
+    // then
+    assert(expectedColumns.forall(expectedCol =>
+      results("ebisearchEvidence").columns.contains(expectedCol)))
+
+    assert(expectedColumns.forall(expectedCol =>
+      results("ebisearchAssociations").columns.contains(expectedCol)))
+  }
+
+}
