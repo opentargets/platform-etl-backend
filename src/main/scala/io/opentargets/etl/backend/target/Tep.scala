372cd4a0e534bac890faf256311c7671db1afc24
Jarrod Baker <43906063+JarrodBaker@users.noreply.github.com>
2021-09-07 11:33:16 +0100

    1742 update tep (#198)
    * Update TEP to use new input/output schema.
    
    Relates to opentargets/platform#1742.
    
    * Add note explaining TEP duplicate work-around.

--- src/main/scala/io/opentargets/etl/backend/target/Tep.scala
+++ src/main/scala/io/opentargets/etl/backend/target/Tep.scala
@@ -1,21 +1,32 @@
 package io.opentargets.etl.backend.target
 
 import com.typesafe.scalalogging.LazyLogging
-import io.opentargets.etl.backend.spark.Helpers.nest
+import org.apache.spark.sql.functions.{col, struct}
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 case class TepWithId(ensemblId: String, tep: Tep)
-case class Tep(name: String, uri: String)
+
+case class Tep(description: String, therapeutic_area: String, url: String)
 
 object Tep extends LazyLogging {
 
+  /**
+    * @fixme see [discussion](https://app.zenhub.com/workspaces/open-targets-issue-tracker-58a421fd8c85e652659a1486/issues/opentargets/platform/1742)
+    *        of dropping duplicates. This is a temporary work-around and in 21.12 we'll update this to collect an array of TEP
+    *        objects.
+    */
   def apply(df: DataFrame)(implicit ss: SparkSession): Dataset[TepWithId] = {
     import ss.implicits._
     logger.info("Transforming Tep inputs")
-    df.withColumnRenamed("Ensembl_id", "ensemblId")
-      .withColumnRenamed("OT_Target_name", "name")
-      .withColumnRenamed("URI", "uri")
-      .transform(nest(_, List("name", "uri"), "tep"))
+    df.select(
+        col("gene_id") as "ensemblId",
+        struct(
+          col("description"),
+          col("disease") as "therapeutic_area",
+          col("TEP_url") as "url"
+        ) as "tep"
+      )
+      .dropDuplicates("ensemblId")
       .as[TepWithId]
   }
 
