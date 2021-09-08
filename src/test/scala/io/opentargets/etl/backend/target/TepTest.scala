3613ff92e2f17053bd07a03b0c2f0319be060026
Jarrod Baker <43906063+JarrodBaker@users.noreply.github.com>
2021-09-08 10:45:39 +0100

    1742 update tep 2 (#199)
    * Remove duplicated call to `filterAndSortProteinIds`
    
    * Update TEP tests.
    
    * Update TEP to use new schema and resolve ensg id in ETL.
    
    Update method `generateEnsgToSymbolLookup` to include additional fields
    for 'uniprot' and 'hgnc' and use arrays. By splitting out the name by
    source we can create a priority by matching on specific fields in a
    specific order.
    
    * Update Safety to use new `ensemblIdLookupDf` implementation.
    
    * Update reference.conf with latest tep file.

--- src/test/scala/io/opentargets/etl/backend/target/TepTest.scala
+++ src/test/scala/io/opentargets/etl/backend/target/TepTest.scala
@@ -8,13 +8,12 @@ class TepTest extends EtlSparkUnitTest {
   "Raw Tep file" should "be converted to dataset without loss" in {
     // given
     val df = sparkSession.read
-      .option("multiLine", true)
       .json(this.getClass.getResource("/target/tep_test.json").getPath)
     // when
     val results = Tep(df)
 
     // then
-    results.count() should be(df.select("gene_id").distinct.count())
+    results.count() should be(df.select("targetFromSource").distinct.count())
   }
 
 }
