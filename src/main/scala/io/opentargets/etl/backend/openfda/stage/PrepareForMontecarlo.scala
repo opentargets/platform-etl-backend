0ec9d3fc0962d135d6246a21c915e3d426af7178
Manuel Bernal Llinares <mbdebian@gmail.com>
2021-10-20 13:37:48 +0100

    add substep logging information

--- src/main/scala/io/opentargets/etl/backend/openfda/stage/PrepareForMontecarlo.scala
+++ src/main/scala/io/opentargets/etl/backend/openfda/stage/PrepareForMontecarlo.scala
@@ -1,15 +1,18 @@
 package io.opentargets.etl.backend.openfda.stage
 
+import com.typesafe.scalalogging.LazyLogging
 import io.opentargets.etl.backend.ETLSessionContext
+import io.opentargets.etl.backend.openfda.stage.PrepareSummaryStatistics.logger
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, lit, log}
 
-object PrepareForMontecarlo {
+object PrepareForMontecarlo extends LazyLogging {
   def apply(fdaData: DataFrame,
             targetDimensionStatsColId: String)
            (implicit context: ETLSessionContext) = {
     import context.sparkSession.implicits._
 
+    logger.info(s"Prepare data for Montecarlo on target dimension stats '${targetDimensionStatsColId}'")
     // total unique report ids
     val uniqReports: Long = fdaData.select("safetyreportid").distinct.count
     val doubleAgg = fdaData
