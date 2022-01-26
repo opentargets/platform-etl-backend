74f2a07b7fc9a0bb86b152b83802ea3fc33a5b7e
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-01-26 12:45:00 +0000

    Resolves opentargets/platform#1896

--- src/main/scala/io/opentargets/etl/Main.scala
+++ src/main/scala/io/opentargets/etl/Main.scala
@@ -1,11 +1,17 @@
 package io.opentargets.etl
 
+import com.google.api.gax.paging.Page
+import com.google.cloud.storage.{Blob, Storage, StorageOptions}
+import com.google.cloud.storage.Storage.BlobListOption
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.util._
 import io.opentargets.etl.backend._
 import io.opentargets.etl.backend.drug.Drug
 import io.opentargets.etl.backend.graph.EtlDag
+import io.opentargets.etl.common.GoogleStorageHelpers
+
+import scala.collection.JavaConverters
 
 object ETL extends LazyLogging {
 
@@ -67,12 +73,55 @@ object ETL extends LazyLogging {
     logger.info(s"finished to run step ($step)")
   }
 
+  /**
+    * Identify steps whose outputs already exist in GCP.
+    * */
+  def stepsWithExistingOuputs(implicit ctx: ETLSessionContext): Set[String] = {
+    lazy val outputPaths: Map[String, String] = Map(
+      "disease" -> ctx.configuration.disease.outputs.diseases.path,
+      "reactome" -> ctx.configuration.reactome.output.path,
+      "expression" -> ctx.configuration.expression.output.path,
+      "go" -> ctx.configuration.geneOntology.output.path,
+      "target" -> ctx.configuration.target.outputs.target.path,
+      "interaction" -> ctx.configuration.interactions.outputs.interactions.path,
+      "targetValidation" -> ctx.configuration.targetValidation.output.succeeded.path,
+      "evidence" -> ctx.configuration.evidences.outputs.succeeded.path,
+      "association" -> ctx.configuration.associations.outputs.directByDatatype.path,
+      "associationOTF" -> ctx.configuration.aotf.outputs.clickhouse.path,
+      "search" -> ctx.configuration.search.outputs.diseases.path,
+      "drug" -> ctx.configuration.drug.outputs.drug.path,
+      "knownDrug" -> ctx.configuration.knownDrugs.output.path,
+      "ebisearch" -> ctx.configuration.ebisearch.outputs.ebisearchEvidence.path,
+      "fda" -> ctx.configuration.openfda.outputs.fdaResults.path,
+    )
+
+    val storage: Storage = StorageOptions.getDefaultInstance.getService
+
+    val (bucket, blob) = GoogleStorageHelpers.pathToBucketBlob(ctx.configuration.common.output)
+
+    val blobs: Page[Blob] =
+      storage.list(bucket, BlobListOption.currentDirectory(), BlobListOption.prefix(blob))
+
+    val existingOutputs: Set[String] = JavaConverters
+      .asScalaIteratorConverter(blobs.iterateAll().iterator())
+      .asScala
+      .filter(_.isDirectory)
+      .map(dir => s"gs://${dir.getName}")
+      .toSet
+    outputPaths.mapValues(existingOutputs.contains).keySet
+  }
+
   def apply(steps: Seq[String]): Unit = {
 
     ETLSessionContext() match {
       case Right(otContext) =>
         implicit val ctxt: ETLSessionContext = otContext
 
+        val completedSteps =
+          if (otContext.configuration.sparkSettings.ignoreIfExists && otContext.configuration.sparkSettings.writeMode == "ignore")
+            stepsWithExistingOuputs
+          else Set.empty[String]
+        logger.info(s"Steps already completed and not to be executed again: $completedSteps")
         // build ETL DAG graph
         val etlDag = new EtlDag[String](otContext.configuration.etlDag.steps)
 
@@ -83,7 +132,10 @@ object ETL extends LazyLogging {
 
         logger.info(s"Steps to execute: $etlSteps")
 
-        etlSteps.foreach { step =>
+        val filteredSteps = etlSteps.filter(s => !completedSteps.contains(s))
+        logger.info(s"Steps to execute after filtering: $filteredSteps")
+
+        filteredSteps.foreach { step =>
           logger.debug(s"step to run: '$step'")
           ETL.applySingleStep(step)
         }
