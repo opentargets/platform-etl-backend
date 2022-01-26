0ca478e85872b98ffd42706c933c1d8b07563585
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-01-26 12:45:00 +0000

    Resolves opentargets/platform#1896

---
+++ src/main/scala/io/opentargets/etl/common/GoogleStorageHelpers.scala
@@ -0,0 +1,18 @@
+package io.opentargets.etl.common;
+
+object GoogleStorageHelpers {
+
+  /**
+    * @path path to google storage blob.
+    * @return a tuple of (bucket, blob) for a given path.
+    * */
+  def pathToBucketBlob(path: String): (String, String) = {
+    require(path.trim.startsWith("gs://"))
+    val noPrefix = path.drop(5)
+    val bucketAndBlob = noPrefix.splitAt(noPrefix.prefixLength(_ != '/'))
+    (bucketAndBlob._1, bucketAndBlob._2.drop(1)) // remove leading '/'
+  }
+
+  def isGoogleStoragePath(path: String): Boolean = path.startsWith("gs://")
+
+}
