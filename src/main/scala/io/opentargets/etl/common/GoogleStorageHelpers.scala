b5bce77d859d91e33d4e2646d1d9145f21f96cf0
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-01-31 11:37:29 +0000

    Fix bug in google storage interaction.
    Adding '/' so gs lists child directories which are the output paths and
    updating how the set matches so we correctly identify completed steps.

--- src/main/scala/io/opentargets/etl/common/GoogleStorageHelpers.scala
+++ src/main/scala/io/opentargets/etl/common/GoogleStorageHelpers.scala
@@ -10,7 +10,7 @@ object GoogleStorageHelpers {
     require(path.trim.startsWith("gs://"))
     val noPrefix = path.drop(5)
     val bucketAndBlob = noPrefix.splitAt(noPrefix.prefixLength(_ != '/'))
-    (bucketAndBlob._1, bucketAndBlob._2.drop(1)) // remove leading '/'
+    (bucketAndBlob._1, bucketAndBlob._2.drop(1) + "/") // remove leading '/', add tailing '/' so we get the output dir.
   }
 
   def isGoogleStoragePath(path: String): Boolean = path.startsWith("gs://")
