2ee8bdb4e28c40f633454fa22a7abd524ce3b96b
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-03-10 16:35:46 +0000

    V2G: Adding VEP

--- src/main/scala/io/opentargets/etl/backend/genetics/VariantGene.scala
+++ src/main/scala/io/opentargets/etl/backend/genetics/VariantGene.scala
@@ -4,8 +4,23 @@ import com.typesafe.scalalogging.LazyLogging
 import io.opentargets.etl.backend.ETLSessionContext
 import io.opentargets.etl.backend.spark.IoHelpers.IOResources
 import io.opentargets.etl.backend.spark.{IOResource, IoHelpers}
-import org.apache.spark.sql.functions.{col, map_from_entries, regexp_extract, split}
+import org.apache.spark.sql.functions.{
+  array_max,
+  col,
+  collect_set,
+  element_at,
+  explode,
+  lit,
+  map_filter,
+  map_from_entries,
+  map_keys,
+  regexp_extract,
+  split,
+  struct
+}
 import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.:+
 
 object VariantGene extends LazyLogging {
 
@@ -33,30 +48,51 @@ object VariantGene extends LazyLogging {
     val vepRawDf: DataFrame = inputs("vep").data
     val intervalRawDf: DataFrame = inputs("interval").data
 
-    val variantIdx = variantRawDf
+    val idxCols = Seq("chr_id", "position", "ref_allele", "alt_allele").map(col)
+    val groupingCols = idxCols :+ col("vep_gene_id")
+
+    val variantIdx: DataFrame = variantRawDf
       .select(
         col("chr_id"),
         col("position"),
         col("ref_allele"),
         col("alt_allele"),
-        col("gene_id"),
-        col("transcript_consequences")
+        explode(col("vep")) as "vep"
       )
-      .filter(col("transcript_consequences").isNotNull)
-    // veps
-    val vepDf = vepRawDf
-      .filter(col("v2g_score").isNotNull)
       .select(
-        regexp_extract(col("Accession"), ".+/(.+)", 1) as "accession",
-        col("Term") as "term",
-        col("Description") as "description",
-        col("Display term") as "display_term",
-        col("IMPACT") as "impact",
-        col("v2g_score"),
-        col("eco_score"),
+        idxCols :+ col("vep.*"): _*
       )
 
+    // veps
+    val vepConsequencesDf = vepRawDf
+      .filter(col("v2g_score").isNotNull)
+      .select(
+        col("Term") as "fpred_label",
+        col("v2g_score") as "fpred_score"
+      )
+
+    val variantGeneVepDf = variantIdx
+      .select(groupingCols :+ (explode(col("fpred_labels")) as "fpred_label"): _*)
+      .join(vepConsequencesDf, Seq("fpred_label"), "left_outer")
+      .groupBy(groupingCols: _*)
+      .agg(collect_set(struct(col("fpred_score"), col("fpred_label"))) as "f")
+      .select(
+        idxCols ++ Seq(
+          map_filter(map_from_entries(col("f")), (k, _) => k > 0F).as("score_label_map"),
+          col("f.fpred_score") as "fpred_scores",
+          col("f.fpred_label") as "fpred_labels",
+          lit("fpred").as("type_id"),
+          lit("vep").as("source_id"),
+          lit("unspecified").as("feature"),
+        ): _*
+      )
+      .withColumn("fpred_max_score", array_max(map_keys(col("score_label_map"))))
+      .withColumn("fpred_max_label", element_at(col("score_label_map"), col("fpred_max_score")))
+      .filter(col("fpred_max_score").isNotNull)
+      .drop("score_label_map")
+
     val variantGeneIdx: DataFrame = ???
+
     val outputs = Map(
       "variantGene" -> IOResource(variantGeneIdx, configuration.outputs.variantGene)
     )
