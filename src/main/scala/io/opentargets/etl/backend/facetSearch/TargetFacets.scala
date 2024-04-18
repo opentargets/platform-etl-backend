87af3b69d7c3ff69a6754aa400997ca58698c859
James Hayhurst <hayhurst.jd@gmail.com>
2024-04-18 15:40:44 +0100

    added Subcellular Location facets

--- src/main/scala/io/opentargets/etl/backend/facetSearch/TargetFacets.scala
+++ src/main/scala/io/opentargets/etl/backend/facetSearch/TargetFacets.scala
@@ -1,9 +1,14 @@
 package io.opentargets.etl.backend.facetSearch
 
 import io.opentargets.etl.backend.target.TractabilityWithId
+import io.opentargets.etl.backend.spark.Helpers.LocationAndSource
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.sql.functions.{array, col, collect_set, lit, map_values, typedLit, when}
 import org.apache.spark.sql.{Column, DataFrame, Dataset, SparkSession}
+
+case class SubcellularLocationWithId(ensemblGeneId: String,
+                                     subcellularLocations: Array[LocationAndSource]
+)
 
 /** Object TargetFacets is used to compute various facets of targets.
   */
@@ -99,6 +104,29 @@ object TargetFacets extends LazyLogging {
   ): Dataset[Facets] = {
     logger.info("Computing approved name facets")
     computeSimpleFacet(targetsDF, "approvedName", "Approved Name", "id")
+  }
+
+  def computeSubcellularLocationsFacets(targetsDF: DataFrame)(implicit
+      sparkSession: SparkSession
+  ): Dataset[Facets] = {
+    import sparkSession.implicits._
+    logger.info("Computing subcellular locations facets")
+    val subcellularLocationWithId: Dataset[SubcellularLocationWithId] =
+      targetsDF
+        .select(col("id").as("ensemblGeneId"), col("subcellularLocations"))
+        .where(col("subcellularLocations").isNotNull)
+        .as[SubcellularLocationWithId]
+    val subcellularLocationsFacets: Dataset[Facets] = subcellularLocationWithId
+      .flatMap(row =>
+        row.subcellularLocations.map(s =>
+          (row.ensemblGeneId, s.location, "Subcellular Location", s.termSl)
+        )
+      )
+      .toDF("ensemblGeneId", "label", "category", "datasourceId")
+      .groupBy("label", "category", "datasourceId")
+      .agg(collect_set("ensemblGeneId").as("entityIds"))
+      .as[Facets]
+    subcellularLocationsFacets
   }
 
   /** Compute simple facet dataset for the given DataFrame, setting the datasourceId to null.
