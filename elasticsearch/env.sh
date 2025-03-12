cfe8ebe1e0158df93b68c880d2d6a9cf2550e039
ricardo <remo87@gmail.com>
2025-03-12 16:26:15 +0000

    remove external schemas

--- elasticsearch/env.sh
+++
@@ -1,3 +0,0 @@
-export PREFIX="path to data"
-export ES=localhost:9200
-export INDEX_SETTINGS="foo/bar/index_settings.json"
--- elasticsearch/load_interaction_evidence.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#cat "$1" | elasticsearch_loader --es-host "http://localhost:9200" --index-settings-file "index_settings.json" --bulk-size 5000 --index cancerbiomarker --type cancerbiomarker json --json-lines -
-
-export INDEX_NAME="interaction_evidence"
-export INPUT="${PREFIX}/interactionEvidence"
-
-./load_jsons.sh
