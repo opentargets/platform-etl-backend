bf38cc75d1747859d1770634c8caa110e66cf0ff
ricardo <remo87@gmail.com>
2025-03-24 12:18:01 +0000

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,22 +17,22 @@ object Dependencies {
   ).flatten
 
   lazy val cli = Seq(
-    "com.monovore" %% "decline-effect" % "2.3.1"
+    "com.monovore" %% "decline-effect" % "2.5.0"
   )
 
-  lazy val betterFiles = Seq("com.github.pathikrit" %% "better-files-akka" % "3.9.1")
+  lazy val betterFiles = Seq("com.github.pathikrit" %% "better-files-akka" % "3.9.2")
 
   lazy val configDeps = Seq(
-    "com.github.pureconfig" %% "pureconfig" % "0.17.1"
+    "com.github.pureconfig" %% "pureconfig" % "0.17.8"
   )
 
   lazy val loggingDeps = Seq(
-    "ch.qos.logback" % "logback-classic" % "1.4.4",
-    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
+    "ch.qos.logback" % "logback-classic" % "1.5.18",
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
   )
 
   lazy val graphDeps = Seq(
-    "org.jgrapht" % "jgrapht-core" % "1.4.0"
+    "org.jgrapht" % "jgrapht-core" % "1.5.2"
   )
 
   lazy val sparkVersion = "3.2.4"
@@ -52,25 +52,25 @@ object Dependencies {
       }
     Seq(
       "com.github.fommil.netlib" % "all" % "1.1.2" pomOnly (),
-      "com.databricks" %% "spark-xml" % "0.11.0"
+      "com.databricks" %% "spark-xml" % "0.18.0"
     ) ++ sparkDeps
   }
 
-  lazy val testVersion = "3.2.2"
+  lazy val testVersion = "3.2.19"
   lazy val testingDeps = Seq(
     "org.scalactic" %% "scalactic" % testVersion % Test,
     "org.scalatest" %% "scalatest" % testVersion % Test,
-    "org.scalamock" %% "scalamock" % "5.1.0" % Test
+    "org.scalamock" %% "scalamock" % "6.2.0" % Test
   )
 
-  lazy val typeSafeConfig = Seq("com.typesafe" % "config" % "1.4.1")
+  lazy val typeSafeConfig = Seq("com.typesafe" % "config" % "1.4.3")
 
   lazy val gcpEtl = Seq(
-    "com.google.cloud" % "google-cloud-dataproc" % "4.2.0" % "provided",
-    "com.google.cloud" % "google-cloud-storage" % "2.4.2"
+    "com.google.cloud" % "google-cloud-dataproc" % "4.57.0" % "provided",
+    "com.google.cloud" % "google-cloud-storage" % "2.50.0"
   )
 
-  lazy val johnSVersion = "3.3.4"
+  lazy val johnSVersion = "5.5.3"
   lazy val johnS = Seq(
     "com.johnsnowlabs.nlp" % "spark-nlp_2.12" % johnSVersion
   )
