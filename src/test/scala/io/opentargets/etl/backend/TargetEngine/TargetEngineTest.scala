bb3176d6af130533505a0de18a157cbf5f53b5eb
project-defiant <szymonszyszkowski@gmail.com>
2025-05-19 18:47:49 +0100

    format

--- src/test/scala/io/opentargets/etl/backend/TargetEngine/TargetEngineTest.scala
+++ src/test/scala/io/opentargets/etl/backend/TargetEngine/TargetEngineTest.scala
@@ -11,7 +11,9 @@ object TargetEngineTest {
   def createQuerySetDf(implicit context: ETLSessionContext): DataFrame = {
     val rows = Seq(Row("ENSG00000083454"), Row("ENSG00000092201"), Row("ENSG00000105707"))
     val schema = StructType(StructField("targetId", StringType, nullable = false) :: Nil)
-    context.sparkSession.createDataFrame(context.sparkSession.sparkContext.parallelize(rows), schema)
+    context.sparkSession.createDataFrame(context.sparkSession.sparkContext.parallelize(rows),
+                                         schema
+    )
   }
 
   def createMouseDf(implicit context: ETLSessionContext): DataFrame = {
@@ -26,31 +28,50 @@ object TargetEngineTest {
       Row(
         "ENSG00000092201",
         Seq(
-          Row("MP:0005386", "behavior/neurological phenotype" ),
+          Row("MP:0005386", "behavior/neurological phenotype")
         )
       ),
       Row(
         "ENSG00000083454",
         Seq(
           Row("MP:0005397", "hematopoietic system phenotype"),
-          Row("MP:0005387", "immune system phenotype"),
+          Row("MP:0005387", "immune system phenotype")
         )
       )
     )
-    val modelPhenotypeClassSchema = StructType(StructField("id", StringType, nullable = false) :: StructField("label", StringType, nullable = false) :: Nil)
+    val modelPhenotypeClassSchema = StructType(
+      StructField("id", StringType, nullable = false) :: StructField("label",
+                                                                     StringType,
+                                                                     nullable = false
+      ) :: Nil
+    )
     val schema = StructType(
       StructField("targetFromSourceId", StringType, nullable = false) ::
         StructField("modelPhenotypeClasses", ArrayType(modelPhenotypeClassSchema)) ::
-      Nil)
-    context.sparkSession.createDataFrame(context.sparkSession.sparkContext.parallelize(rows), schema)
+        Nil
+    )
+    context.sparkSession.createDataFrame(context.sparkSession.sparkContext.parallelize(rows),
+                                         schema
+    )
   }
   def createMousePhenoScoreDf(implicit context: ETLSessionContext): DataFrame = {
-    val rows = Seq(Row("MP:0003631", 0.75), Row("MP:0005386", 0.75), Row("MP:0005377", 0.5), Row("MP:0005397", 0.5), Row("MP:0005387", 0.5))
-    val schema = StructType(StructField("id", StringType, nullable = false) :: StructField("score", DoubleType, nullable=false):: Nil)
-    context.sparkSession.createDataFrame(context.sparkSession.sparkContext.parallelize(rows), schema)
+    val rows = Seq(Row("MP:0003631", 0.75),
+                   Row("MP:0005386", 0.75),
+                   Row("MP:0005377", 0.5),
+                   Row("MP:0005397", 0.5),
+                   Row("MP:0005387", 0.5)
+    )
+    val schema = StructType(
+      StructField("id", StringType, nullable = false) :: StructField("score",
+                                                                     DoubleType,
+                                                                     nullable = false
+      ) :: Nil
+    )
+    context.sparkSession.createDataFrame(context.sparkSession.sparkContext.parallelize(rows),
+                                         schema
+    )
   }
 }
-
 
 class TargetEngineTest extends EtlSparkUnitTest {
 
@@ -60,15 +81,21 @@ class TargetEngineTest extends EtlSparkUnitTest {
     val querySetDf: DataFrame = TargetEngineTest.createQuerySetDf(ctx)
     val mouseDf: DataFrame = TargetEngineTest.createMouseDf(ctx)
     val mousePhenoScoresDf: DataFrame = TargetEngineTest.createMousePhenoScoreDf(ctx)
-    val expectedColumns = Set("targetId", "target_id_", "score", "harmonicSum", "maxHarmonicSum", "maximum", "scaledHarmonicSum", "negScaledHarmonicSum")
+    val expectedColumns = Set("targetId",
+                              "target_id_",
+                              "score",
+                              "harmonicSum",
+                              "maxHarmonicSum",
+                              "maximum",
+                              "scaledHarmonicSum",
+                              "negScaledHarmonicSum"
+    )
 
     // when
     val results: DataFrame = mousemodQuery(querySetDf, mouseDf, mousePhenoScoresDf)
 
     // then
     assert(expectedColumns.forall(expectedCol => results.columns.contains(expectedCol)))
-
-
 
   }
 }
