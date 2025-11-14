55fffd172f7fa75bc3e99714b6015c18807a86b5
ricardo <remo87@gmail.com>
2025-11-14 10:51:54 +0000

    mock config

--- src/test/scala/io/opentargets/etl/backend/target/HgncTest.scala
+++ src/test/scala/io/opentargets/etl/backend/target/HgncTest.scala
@@ -1,6 +1,7 @@
 package io.opentargets.etl.backend.target
 
-import io.opentargets.etl.backend.Configuration.OTConfig
+import io.opentargets.etl.backend.Configuration.{OTConfig, TargetSection}
+import io.opentargets.etl.backend.spark.IoHelpers.IOResourceConfigurations
 import io.opentargets.etl.backend.{Configuration, EtlSparkUnitTest}
 import io.opentargets.etl.backend.target.HgncTest.hgncRawDf
 import org.apache.spark.sql.functions.col
@@ -17,7 +18,6 @@ object HgncTest {
 }
 
 class HgncTest extends EtlSparkUnitTest {
-  val conf: ConfigReader.Result[OTConfig] = Configuration.config
 
   val selectAndRenameFields: PrivateMethod[Dataset[Hgnc]] =
     PrivateMethod[Dataset[Hgnc]]('selectAndRenameFields)
@@ -26,15 +26,36 @@ class HgncTest extends EtlSparkUnitTest {
     PrivateMethod[Dataset[Hgnc]]('prepareInputDataFrame)
 
   "HGNC" should "convert raw dataframe into HGCN objects without loss" in {
-    conf map { config =>
-      // given
-      val targetSection = config.steps.target
-      val df = Hgnc invokePrivate prepareInputDataFrame(hgncRawDf, targetSection)
-      // when
-      val results = Hgnc invokePrivate selectAndRenameFields(df, sparkSession)
-      // then
-      results.count must equal(hgncRawDf.select(col("ensembl_gene_id")).distinct.count)
-    }
-
+    val targetSection = TargetSection(
+      null.asInstanceOf[IOResourceConfigurations],
+      output = null.asInstanceOf[IOResourceConfigurations],
+      hgncArrayColumns = List(
+        "alias_name",
+        "ccds_id",
+        "ena",
+        "enzyme_id",
+        "gene_group",
+        "gene_group_id",
+        "lsdb",
+        "mane_select",
+        "mgd_id",
+        "omim_id",
+        "prev_name",
+        "prev_symbol",
+        "alias_symbol",
+        "pubmed_id",
+        "refseq_accession",
+        "rgd_id",
+        "rna_central_id",
+        "uniprot_ids"
+      ),
+      hgncOrthologSpecies = List()
+    )
+    // given
+    val df = Hgnc invokePrivate prepareInputDataFrame(hgncRawDf, targetSection)
+    // when
+    val results = Hgnc invokePrivate selectAndRenameFields(df, sparkSession)
+    // then
+    results.count must equal(hgncRawDf.select(col("ensembl_gene_id")).distinct.count)
   }
 }
