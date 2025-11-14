93e2eea7440c3074de27b84d3a0649cfd77dfb24
ricardo <remo87@gmail.com>
2025-11-14 10:40:17 +0000

    update hgnc unit test

--- src/test/scala/io/opentargets/etl/backend/target/HgncTest.scala
+++ src/test/scala/io/opentargets/etl/backend/target/HgncTest.scala
@@ -1,9 +1,11 @@
 package io.opentargets.etl.backend.target
 
-import io.opentargets.etl.backend.EtlSparkUnitTest
+import io.opentargets.etl.backend.Configuration.OTConfig
+import io.opentargets.etl.backend.{Configuration, EtlSparkUnitTest}
 import io.opentargets.etl.backend.target.HgncTest.hgncRawDf
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import pureconfig.ConfigReader
 
 object HgncTest {
   def hgncRawDf(implicit sparkSession: SparkSession): DataFrame =
@@ -15,16 +17,24 @@ object HgncTest {
 }
 
 class HgncTest extends EtlSparkUnitTest {
+  val conf: ConfigReader.Result[OTConfig] = Configuration.config
 
   val selectAndRenameFields: PrivateMethod[Dataset[Hgnc]] =
     PrivateMethod[Dataset[Hgnc]]('selectAndRenameFields)
 
+  val prepareInputDataFrame: PrivateMethod[Dataset[Hgnc]] =
+    PrivateMethod[Dataset[Hgnc]]('prepareInputDataFrame)
+
   "HGNC" should "convert raw dataframe into HGCN objects without loss" in {
-    // given
-    val df = hgncRawDf
-    // when
-    val results = Hgnc invokePrivate selectAndRenameFields(df, sparkSession)
-    // then
-    results.count must equal(hgncRawDf.select(col("ensembl_gene_id")).distinct.count)
+    conf map { config =>
+      // given
+      val targetSection = config.steps.target
+      val df = Hgnc invokePrivate prepareInputDataFrame(hgncRawDf, targetSection)
+      // when
+      val results = Hgnc invokePrivate selectAndRenameFields(df, sparkSession)
+      // then
+      results.count must equal(hgncRawDf.select(col("ensembl_gene_id")).distinct.count)
+    }
+
   }
 }
